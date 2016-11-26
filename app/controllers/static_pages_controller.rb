e61f1725828a2c7232fddce352f6209e4ecfcb07
Rujan shakya <rujanshakya76@gmail.com>
2016-11-26 13:09:03 +0000

    Adding flashmessage

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,37 @@
+class StaticPagesController < ApplicationController
+    def index
+    
+    end
+    
+    def create    
+        @StaticPage = StaticPage.create(StaticPage_params)
+        if @StaticPage.valid?
+          flash[:success] = "Your StaticPage has been posted!"
+        else
+          flash[:alert] = "Woops! Looks like there has been an error!"
+        end
+        redirect_to root_path
+    end
+    
+    def edit
+        @StaticPage = StaticPage.find(params[:id])
+    end
+    
+    def update
+        @StaticPage = StaticPage.find(params[:id])
+        if @StaticPage.update(StaticPage_params)
+          flash[:success] = "The StaticPage has been updated!"
+          redirect_to root_path
+        else
+          flash[:alert] = "Woops! Looks like there has been an error!"
+          redirect_to edit_StaticPage_path(params[:id])
+        end
+    end
+    
+    def destroy
+        @StaticPage = StaticPage.find(params[:id])
+        @StaticPage.destroy
+        flash[:success] = "The StaticPage was successfully deleted!"
+        redirect_to root_path
+    end
+end
