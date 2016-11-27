30d31be18c0edf93339f0e3fc4d276c9ed90bd66
Rujan shakya <rujanshakya76@gmail.com>
2016-11-27 08:55:26 +0000

    Adding the omdbapi

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,7 @@ $(function(){
     e.preventDefault();
 
     $.ajax({
-      url: 'http://www.omdbapi.com/?',
+      url: 'https://www.omdbapi.com/?',
       data: form.serialize()
     })
     .done(function(data){
