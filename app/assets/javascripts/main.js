2cfad1e9f4645f1e53a5fdce95f97eebd1b6b103
Rujan shakya <rujanshakya76@gmail.com>
2016-11-29 14:24:44 +0000

    Adding the movieinfo

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -41,7 +41,7 @@ $('#movies').on('click', 'img', function(e){
   e.preventDefault();
     let id = $(e.target).data('id');
     $.ajax({
-      url: "https://www.omdbapi.com/?",
+      url: "http://www.omdbapi.com/?",
       data: {i: id}   
     })
     .done(function(data){
